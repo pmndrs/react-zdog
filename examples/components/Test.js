8419fa3842547a3397a45fa46aceea655a5b5f2b
Paul Henschel <drcmda@gmail.com>
2019-05-30 23:15:19 +0200

    markdown

--- examples/components/Test.js
+++ examples/components/Test.js
@@ -1,13 +1,13 @@
 import * as ZDOG from 'zdog'
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Illustration, Ellipse, Anchor } from 'react-zdog'
 
-const Content = () => {
-  return (
-    <Illustration>
-      <Ellipse diameter={80} stroke={20} color="#C25" />
-    </Illustration>
-  )
-}
+export default function Content() {
+  const [visible, setVisible] = useState(true)
+  useEffect(() => {
+    // Test taking an object away
+    setTimeout(() => setVisible(false), 1000)
+  }, [])
 
-export default Content
+  return <Illustration>{visible && <Ellipse diameter={80} stroke={20} color="#C25" />}</Illustration>
+}
--- readme.md
+++ readme.md
@@ -1,10 +1,18 @@
 ```jsx
-import { Illustration, Ellipse } from 'react-zdog'
+import * as ZDOG from 'zdog'
+import React, { useState, useEffect } from 'react'
+import { Illustration, Ellipse, Anchor } from 'react-zdog'
 
 const Content = () => {
+  const [visible, setVisible] = useState(true)
+  useEffect(() => {
+    // Test taking an object away
+    setTimeout(() => setVisible(false), 1000)
+  }, [])
+
   return (
     <Illustration>
-      <Ellipse diameter={80} stroke={20} color="#C25" />
+      {visible && <Ellipse diameter={80} stroke={20} color="#C25" />}
     </Illustration>
   )
 }
